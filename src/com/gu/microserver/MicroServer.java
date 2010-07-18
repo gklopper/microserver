c78cb6fc1c1e84f49f919ec2cca869ba8fe22f38
Grant Klopper <grant.klopper@gmail.com>
2010-07-18 15:53:45 +0100

    Added front template

--- src/com/gu/microserver/MicroServer.java
+++ src/com/gu/microserver/MicroServer.java
@@ -9,7 +9,8 @@ public class MicroServer {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         WebAppContext root = new WebAppContext(server,"/", null);
-        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/article/article.html")), "/article");
+        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/templates/article.html")), "/article");
+        root.addServlet(new ServletHolder(new PageServlet("com/gu/microserver/templates/front.html")), "/front");
         root.addServlet(new ServletHolder(new ResourceServlet("text/css", "cssUrl")), "/css");
         root.addServlet(new ServletHolder(new ResourceServlet("text/javascript", "javascriptUrl")), "/js");
         server.start();
